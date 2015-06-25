7cba0041fd3def2a7a8278e741123eebde7d557b
crizlucero <crizlucero@gmail.com>
2015-06-25 16:52:24 -0500

    - Se crea la clase Ant, la cual tendrá las caracteristicas de la hormiga. - Se crea la clase City - Se crea la clase World, donde estarán las ciudades - Se creó una interfaz gráfica

---
+++ Antz/Classes/Ant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antz.Classes
+{
+    class Ant
+    {
+        private int[,] route { get; set; }
+        private int orientation { get; set; }
+        private int pheromone { get; set; }
+
+        public void step()
+        {
+        }
+    }
+}
---
+++ Antz/Classes/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antz.Classes
+{
+    class City
+    {
+        public int Name;
+        public string[] Neighbors;
+    }
+}
